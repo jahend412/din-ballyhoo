83e669d5cf31d4b501b9aaaeaddeeee146aa56ee
Josh Endemann <jahend44@gmail.com>
2024-11-15 20:00:10 -0600

    Update webcastRoutes.js
    Added ID to routes

--- din-ballyhoo-app/backend/routes/webcastRoutes.js
+++ din-ballyhoo-app/backend/routes/webcastRoutes.js
@@ -3,12 +3,19 @@ const webcastController = require('../controllers/webcastController');
 
 const router = express.Router();
 
-router  
-    .get(webcastController.getAllWebcasts)
-    .post(webcastController.createWebcast)
-    .get(webcastController.getWebcastById)
-    .put(webcastController.updateWebcast)
-    .delete(webcastController.deleteWebcast);
+// GET all webcasts
+router.get('/', webcastController.getAllWebcasts);
+
+// POST a new webcast
+router.post('/', webcastController.createWebcast);
+
+// GET a single webcast by ID
+router.get('/:id', webcastController.getWebcastById);
+
+// PUT (update) a webcast by ID
+router.put('/:id', webcastController.updateWebcast);
+
+// DELETE a webcast by ID
+router.delete('/:id', webcastController.deleteWebcast);
 
 module.exports = router;
-
