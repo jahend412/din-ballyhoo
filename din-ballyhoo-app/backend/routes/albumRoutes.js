ab44d4a86cdbd1a1c67e9014d20ea96c6caecb6b
Josh Endemann <jahend44@gmail.com>
2024-11-15 19:26:40 -0600

    Update albumRoutes.js
    Added Comments while trying to debug

--- din-ballyhoo-app/backend/routes/albumRoutes.js
+++ din-ballyhoo-app/backend/routes/albumRoutes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const albumController = require('../controllers/albumController');
-
 const router = express.Router();
 
 router
-    .route('/')
-    .get(albumController.getAllAlbums)
-    .post(albumController.createAlbum)
+    .route('/') // Specify the base path for both methods
+    .get(albumController.getAllAlbums) // Fetch all albums
+    .post(albumController.createAlbum); // Add a new album
 
 router
-    .route('/:id')
-    .get(albumController.getAlbum)
-    .patch(albumController.updateAlbum)
-    .delete(albumController.deleteAlbum);
+    .route('/:id') // Specify the path for operations on a specific album by ID
+    .get(albumController.getAlbumById) // Get a specific album by ID
+    .patch(albumController.updateAlbum) // Update album by ID
+    .delete(albumController.deleteAlbum); // Delete album by ID
 
 module.exports = router;
-
