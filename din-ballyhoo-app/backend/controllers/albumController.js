d46a93b1f92ae2dd43893ee707687f337c271464
Josh Endemann <jahend44@gmail.com>
2024-11-17 17:50:08 -0600

    Integrate APIFeatures into albumController for streamlined query handling
    - Added APIFeatures to albumController to handle filtering, sorting, field limiting, and pagination.
    - Replaced inline query logic with modular APIFeatures methods for consistency and code reuse.
    - Improved the maintainability and readability of album queries.

--- din-ballyhoo-app/backend/controllers/albumController.js
+++ din-ballyhoo-app/backend/controllers/albumController.js
@@ -1,23 +1,17 @@
 const Album = require('../models/albumModel');
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
+const APIFeatures = require('../utils/apiFeatures');
 
 // Get All Albums
 exports.getAllAlbums = async (req, res) => {
     try {
-        const queryObj = { ...req.query }; // Destructure the query object
-        const excludedFields = ['page', 'sort', 'limit', 'fields']; // Define the fields to exclude from the query object
-        excludedFields.forEach((el) => delete queryObj[el]); // Loop through the excluded fields and delete them from the query object
+       // Execute Query
+        const features = new APIFeatures(Album.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
 
-console.log(req.query, queryObj);
-
-        const query = Album.find(queryObj); // Fetch all albums from the database
-
-        const albums = await query; // Await the query
+        const albums = await features.query; // Await the query
         res.status(200).json({
             status: 'success',
             results: albums.length, // Include the number of results
