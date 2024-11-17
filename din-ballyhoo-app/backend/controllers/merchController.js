b5ebe6904e3fd8b0d508f158ae98e49239a15856
Josh Endemann <jahend44@gmail.com>
2024-11-17 17:13:29 -0600

    Update merchController.js
    Add advanced query features for Merch API
    
    - Implement `aliasTopMerch` middleware for top 5 merch items by price.
    - Add filtering, sorting, field limiting, and pagination to `getAllMerch` controller.
    - Utilize advanced filtering operators (gte, gt, lte, lt).
    - Handle edge cases for pagination to avoid invalid page access.
    - Enhance query customization for more efficient and flexible data retrieval.

--- din-ballyhoo-app/backend/controllers/merchController.js
+++ din-ballyhoo-app/backend/controllers/merchController.js
@@ -5,17 +5,59 @@ const app = express();
 
 app.use(express.json());
 
+exports.aliasTopMerch = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-price';
+    req.query.fields = 'name,price';
+    next();
+}
+
 // Get All Merch
 exports.getAllMerch = async (req, res) => {
     try {
+
+// Filtering
 const queryObj = { ...req.query }; // Destructure the query object
 const excludedFields = ['page', 'sort', 'limit', 'fields']; // Define the fields to exclude from the query object
 excludedFields.forEach((el) => delete queryObj[el]); // Loop through the excluded fields and delete them from the query object
 
-console.log(req.query, queryObj);
 
-        const query = await Merch.find(queryObj);
+// Advanced Filtering
+let queryStr = JSON.stringify(queryObj);
+queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
+const query = Merch.find(JSON.parse(queryStr));
+
+// Sorting
+if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query.sort(sortBy);
+} else {
+    query.sort('-createdAt');
+}
+
+// Field Limiting
+if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query.select(fields);
+} else {
+    query.select('-__v');
+}
+
+// Pagination
+const page = req.query.page * 1 || 1;
+const limit = req.query.limit * 1 || 100;
+const skip = (page - 1) * limit;
+
+query.skip(skip).limit(limit);
+
+const numMerch = await Merch.countDocuments();
+if (req.query.page) {
+    if (skip >= numMerch) throw new Error('This page does not exist');
+} 
+        
+// Execute Query
+        
         const merch = await query;
         res.status(200).json({
             status: 'success',
