5605df4814142c44b7a8945a518d280da8549324
Josh Endemann <jahend44@gmail.com>
2024-11-19 21:11:58 -0600

    Update trackRoutes.js
    Trying to upload track.   Getting an error on upload.single is not a function.  Going to work on this tomorrow

--- din-ballyhoo-app/backend/routes/trackRoutes.js
+++ din-ballyhoo-app/backend/routes/trackRoutes.js
@@ -1,17 +1,36 @@
 const express = require('express');
-const trackController = require('../controllers/trackController');
-
 const router = express.Router();
+const upload = require('../utils/multer');  // Make sure this path is correct
+
+const trackController = require('../controllers/trackController');
 
 router
     .route('/')
     .get(trackController.getAllTracks)
-    .post(trackController.createTrack)
+    .post(trackController.createTrack);
    
 router
     .route('/:id')    
     .get(trackController.getTrackById)
     .put(trackController.updateTrack)
     .delete(trackController.deleteTrack);
+
+router
+    .route('/:id/stream', trackController.streamTrackById);
+
+
+// Route to upload a track
+router
+    .post('/upload', upload.single('track'), (err, req, res, next) => {
+    if (err) {
+        // Handle any Multer errors (like file size, incorrect file type, etc.)
+        return res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+    // Proceed to your controller method if there are no errors
+    next();
+}, trackController.uploadTrack);
 
 module.exports = router;
