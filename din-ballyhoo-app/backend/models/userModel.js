823fc84d5ec6baea8dbff5c2ca59b88c965d57dc
Josh Endemann <jahend44@gmail.com>
2024-11-26 19:55:02 -0600

    Update userModel.js

--- din-ballyhoo-app/backend/models/userModel.js
+++ din-ballyhoo-app/backend/models/userModel.js
@@ -10,12 +10,34 @@ const userSchema = new mongoose.Schema({
     enum: ['fan', 'band_member'], 
     required: true 
   },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  passwordChangedAt: Date
 });
 
+
+// Document Middleware
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+// Instance Method to compare password
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
+
+// Instance Method to check if password was changed after token was issued
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
+};
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
