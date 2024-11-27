8cc848f64dae3500dae0c4b8b17cec3a21d329a8
Josh Endemann <jahend44@gmail.com>
2024-11-26 19:54:51 -0600

    Update authController.js
    - Added the Document middleware.
    -  Added the instance method to check if password has been changed.

--- din-ballyhoo-app/backend/controllers/authController.js
+++ din-ballyhoo-app/backend/controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const { promisify } = require('util');
 
 const signToken = id => {
     return jwt({ id }, process.env.JWT_SECRET, {
@@ -48,4 +49,31 @@ exports.login = catchAsync(async (req, res, next) => {
         token
     });
 });
+
+// Middleware to protect routes
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    }
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(new AppError('The user belonging to this token does no longer exist.', 401));
+    }
+
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError('User recently changed password! Please log in again.', 401));
+    }
+
+    req.user = currentUser;
+    next();
+}); 
 
