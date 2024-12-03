bc2bf2385cf01006438451f7756c374eb2ff3833
Josh Endemann <jahend44@gmail.com>
2024-12-02 19:24:35 -0600

    Update authController.js
    Add update password functionality with current password validation and JWT login

--- din-ballyhoo-app/backend/controllers/authController.js
+++ din-ballyhoo-app/backend/controllers/authController.js
@@ -201,7 +201,21 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 });
 
 // Update Password
-// Get user from collection
-// Check if posted current password is correct
-// If so, update password
-// Log user in, send JWT
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate will NOT work as intended!
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
+});
