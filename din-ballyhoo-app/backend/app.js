b8f8df57483f0b844b8b9c4fe632235b6e968c74
Josh Endemann <jahend44@gmail.com>
2024-12-02 19:50:37 -0600

    Update app.js
    implement xss-clean for data sanitation against XSS attacks

--- din-ballyhoo-app/backend/app.js
+++ din-ballyhoo-app/backend/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,6 +38,9 @@ app.use(express.json({ limit: '100kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
