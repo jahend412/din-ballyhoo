4d41ae807bfc01f41c7804109af8c18a5ba1ca49
Josh Endemann <jahend44@gmail.com>
2024-12-10 19:24:28 -0600

    Create commentRoutes.js
    Added routes for creating and retrieving comments. Integrated authentication middleware to protect routes.

---
+++ din-ballyhoo-app/backend/routes/commentRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const commentController = require('../controllers/commentController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router
+  .route('/')
+  .get(commentController.getAllComments)
+  .post(commentController.createComment);
+
+module.exports = router;
