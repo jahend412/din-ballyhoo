eaf77de01578510b4543fb57872e44199407fbff
Josh Endemann <jahend44@gmail.com>
2024-12-10 19:16:50 -0600

    Create commentModel.js
    Added `Comment` model with references to `User`, `Track`, `Album`, `Show`, and `Webcast`. Each comment is required to belong to a specific user and associated entity.

---
+++ din-ballyhoo-app/backend/models/commentModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  commment: {
+    type: String,
+    required: [true, 'Comment is required'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Comment must belong to a user'],
+  },
+  track: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Track',
+    required: [true, 'Comment must belong to a track'],
+  },
+  album: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Album',
+    required: [true, 'Comment must belong to an album'],
+  },
+  show: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Show',
+    required: [true, 'Comment must belong to a show'],
+  },
+  webcast: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Webcast',
+    required: [true, 'Comment must belong to a webcast'],
+  },
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
