e27f6de1fbe2d46a4895db7302d1cdfd15ef6f72
Josh Endemann <jahend44@gmail.com>
2024-12-12 14:20:26 -0600

    Update commentRoutes.js
    Changed to appropriate routes

--- din-ballyhoo-app/backend/routes/commentRoutes.js
+++ din-ballyhoo-app/backend/routes/commentRoutes.js
@@ -4,16 +4,18 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// Protect all routes below this middleware
 router.use(authController.protect);
 
+// Routes for creating, getting, updating, and deleting comments
 router
-  .route('/')
-  .get(commentController.getAllComments)
-  .post(commentController.createComment);
+  .route('/:entityType/:entityId/comments') // for creating and retrieving comments for an entity
+  .get(commentController.getCommentsForEntity) // Get all comments for a specific entity
+  .post(commentController.createComment); // Create a new comment for a specific entity
 
 router
-  .route('/:id')
-  .patch(commentController.updateComment)
-  .delete(commentController.deleteComment);
+  .route('/:entityType/:entityId/comments/:id') // for updating and deleting a specific comment
+  .patch(commentController.updateComment) // Update a specific comment
+  .delete(commentController.deleteComment); // Delete a specific comment
 
 module.exports = router;
