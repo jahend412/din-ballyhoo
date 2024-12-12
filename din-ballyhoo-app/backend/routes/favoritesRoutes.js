5931ab8755b598f81ce2e0597adf2a99ce9e7b85
Josh Endemann <jahend44@gmail.com>
2024-12-12 14:04:13 -0600

    Add favorites routes with authentication middleware
    - Created routes for managing user favorites: GET, POST, and DELETE.
    - Integrated `getFavorites`, `addToFavorite`, and `removeFromFavorites` controllers.
    - Secured all routes with authentication middleware using `AuthController.protect`.

---
+++ din-ballyhoo-app/backend/routes/favoritesRoutes.js
@@ -0,0 +1,14 @@
+const Express = require('express');
+const FavoritesController = require('../controllers/favoritesController');
+const AuthController = require('../controllers/authController');
+
+const Router = Express.Router();
+
+Router.use(AuthController.protect);
+
+Router.route('/')
+  .get(FavoritesController.getFavorites)
+  .post(FavoritesController.addToFavorite)
+  .delete(FavoritesController.removeFromFavorites);
+
+module.exports = Router;
