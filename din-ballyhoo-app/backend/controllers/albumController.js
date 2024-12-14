f8394703356fbd774b0af1ccd01c2c4a15828a81
Josh Endemann <jahend44@gmail.com>
2024-12-13 20:51:46 -0600

    Update albumController.js
    Added the factory from handlerFactory and implemented it.

--- din-ballyhoo-app/backend/controllers/albumController.js
+++ din-ballyhoo-app/backend/controllers/albumController.js
@@ -1,69 +1,10 @@
 const Album = require('../models/albumModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
-// Get All Albums
-exports.getAllAlbums = catchAsync(async (req, res, next) => {
-  // Execute Query
-  const features = new APIFeatures(Album.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-
-  const albums = await features.query; // Await the query
-  res.status(200).json({
-    status: 'success',
-    results: albums.length, // Include the number of results
-    data: {
-      albums, // Return the fetched albums here
-    },
-  });
-});
-
-// Create New Album
-exports.createAlbum = catchAsync(async (req, res, next) => {
-  const newAlbum = await Album.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      album: newAlbum,
-    },
-  });
-});
-
-// Get Album By ID
-exports.getAlbumById = catchAsync(async (req, res, next) => {
-  const album = await Album.findById(req.params.id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      album,
-    },
-  });
-});
-
-// Update Album
-exports.updateAlbum = catchAsync(async (req, res, next) => {
-  const album = await Album.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-  res.status(200).json({
-    status: 'success',
-    data: {
-      album,
-    },
-  });
-});
-
-// Delete Album
-exports.deleteAlbum = catchAsync(async (req, res, next) => {
-  const album = await Album.findByIdAndDelete(req.params.id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      album,
-    },
-  });
-});
+exports.getAllAlbums = factory.getAll(Album);
+exports.getAlbum = factory.getOne(Album);
+exports.createAlbum = factory.createOne(Album);
+exports.updateAlbum = factory.updateOne(Album);
+exports.deleteAlbum = factory.deleteOne(Album);
