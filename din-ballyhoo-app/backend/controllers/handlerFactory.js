48c29bb656355403008fedd81612c9e15c7ca31c
Josh Endemann <jahend44@gmail.com>
2024-12-13 20:30:46 -0600

    Update handlerFactory.js
    Add reusable getAll controller for fetching documents with dynamic filtering and query features
    
    This commit introduces a reusable getAll controller function to efficiently handle fetching documents for different models. Key features include:
    - Dynamic Filtering:Supports filtering by albumId, showId, webcastId, or trackId based on the request parameters.
    - Integration with APIFeatures: Leverages the APIFeatures utility for advanced query operations such as filtering, sorting, field limiting, and pagination.
    - Reusable Design: Accepts a Model parameter, allowing it to work with any Mongoose model for scalability and reduced code duplication.
    -Structured Response:Returns results in a consistent JSON format, including the count of documents and the data array.
    
    Why it matters:
    This reusable controller simplifies CRUD operations across the application by centralizing logic for retrieving collections. It improves code maintainability, reduces redundancy, and provides flexibility for expanding query capabilities.

--- din-ballyhoo-app/backend/controllers/handlerFactory.js
+++ din-ballyhoo-app/backend/controllers/handlerFactory.js
@@ -2,11 +2,38 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const APIFeatures = require('../utils/apiFeatures');
 
+// Create a document
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
     res.status(201).json({
       status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+// Get all documents
+exports.getAll = (Model) =>
+  catchAsync(async (req, res) => {
+    let filter = {};
+    if (req.params.albumId) filter = { album: req.params.albumId };
+    if (req.params.showId) filter = { show: req.params.showId };
+    if (req.params.webcastId) filter = { webcast: req.params.webcastId };
+    if (req.params.trackId) filter = { track: req.params.trackId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    // Send response
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
       data: {
         data: doc,
       },
