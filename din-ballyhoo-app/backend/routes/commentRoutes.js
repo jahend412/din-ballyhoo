eb3d2fc5e9b1737e910692d577daf43f3aed79ac
Josh Endemann <jahend44@gmail.com>
2024-12-14 10:37:01 -0600

    Update commentRoutes.js
    Changed the Route.

--- din-ballyhoo-app/backend/routes/commentRoutes.js
+++ din-ballyhoo-app/backend/routes/commentRoutes.js
@@ -9,9 +9,9 @@ router.use(authController.protect);
 
 // Routes for creating, getting, updating, and deleting comments
 router
-  .route('/:entityType/:entityId/comments') // for creating and retrieving comments for an entity
-  .get(commentController.getCommentsForEntity) // Get all comments for a specific entity
-  .post(commentController.createComment); // Create a new comment for a specific entity
+  .route('/:entityType/:entityId')
+  .get(commentController.getCommentsForEntity)
+  .post(commentController.createComment); // POST for creating comments
 
 router
   .route('/:entityType/:entityId/comments/:id') // for updating and deleting a specific comment
