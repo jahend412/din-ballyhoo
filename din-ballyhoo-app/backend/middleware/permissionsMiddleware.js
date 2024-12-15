4adfad322a0262794fcbf7a25a517cfeac8f45c2
Josh Endemann <jahend44@gmail.com>
2024-12-15 11:30:02 -0600

    Update permissionsMiddleware.js
    Added appError to permissions Middleware

--- din-ballyhoo-app/backend/middleware/permissionsMiddleware.js
+++ din-ballyhoo-app/backend/middleware/permissionsMiddleware.js
@@ -1,9 +1,10 @@
-// 3. Middleware for checking permissions
+const appError = require('../utils/appError');
+
 const checkPermissions = (permissions) => {
   return (req, res, next) => {
     if (!req.user || !req.user.permissions.includes(permissions)) {
       return next(
-        new AppError('You do not have permission to perform this action', 403)
+        new appError('You do not have permission to perform this action', 403)
       );
     }
     next();
