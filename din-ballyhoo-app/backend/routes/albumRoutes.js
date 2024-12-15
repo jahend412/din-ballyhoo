081820233e542fd9d6b9b5652dc69f0be0850c31
Josh Endemann <jahend44@gmail.com>
2024-12-14 18:20:59 -0600

    Update albumRoutes.js
    Added checkPermissions to all of the routes!

--- din-ballyhoo-app/backend/routes/albumRoutes.js
+++ din-ballyhoo-app/backend/routes/albumRoutes.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const albumController = require('../controllers/albumController');
+const authController = require('../controllers/authController');
+const checkPermissions = require('../middleware/permissionsMiddleware');
+
 const router = express.Router();
+
+// Protect all routes below this middleware
+router.use(authController.protect);
 
 router
   .route('/') // Specify the base path for both methods
-  .get(albumController.getAllAlbums) // Fetch all albums
-  .post(albumController.createAlbum); // Add a new album
+  .get(checkPermissions('view-album'), albumController.getAllAlbums) // Fetch all albums
+  .post(checkPermissions('create-album'), albumController.createAlbum); // Add a new album
 
 router
   .route('/:id') // Specify the path for operations on a specific album by ID
-  .get(albumController.getAlbum) // Get a specific album by ID
-  .patch(albumController.updateAlbum) // Update album by ID
-  .delete(albumController.deleteAlbum); // Delete album by ID
+  .get(checkPermissions('view-album'), albumController.getAlbum) // Get a specific album by ID
+  .patch(checkPermissions('edit-album'), albumController.updateAlbum) // Update album by ID
+  .delete(checkPermissions('delete-album'), albumController.deleteAlbum); // Delete album by ID
 
 module.exports = router;
