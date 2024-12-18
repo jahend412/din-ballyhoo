95cbb4cbe6c8f8f9ee0f7d6f3cc27c70e70f3ed0
Josh Endemann <jahend44@gmail.com>
2024-12-17 19:00:59 -0600

    Update showController.js
    Add Path to ratings and favorites.

--- din-ballyhoo-app/backend/controllers/showController.js
+++ din-ballyhoo-app/backend/controllers/showController.js
@@ -4,7 +4,16 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getAllShows = factory.getAll(Shows);
-exports.getShow = factory.getOne(Shows);
+exports.getShow = factory.getOne(Shows, [
+  {
+    path: 'ratings', // Virtual field for ratings
+    select: 'rating fan createdAt', // Only include these fields in the populated ratings
+  },
+  {
+    path: 'favorites', // Virtual field for favorites
+    select: 'fan createdAt', // Only include these fields in the populated favorites
+  },
+]);
 exports.createShow = factory.createOne(Shows);
 exports.updateShow = factory.updateOne(Shows);
 exports.deleteShow = factory.deleteOne(Shows);
