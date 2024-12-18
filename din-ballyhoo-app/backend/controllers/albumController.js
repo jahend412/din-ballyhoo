5791fc29fe7b430aa2525474438634657e03205d
Josh Endemann <jahend44@gmail.com>
2024-12-17 19:00:48 -0600

    Update albumController.js
    Add the Path of favorites and ratings to find AlbumById.

--- din-ballyhoo-app/backend/controllers/albumController.js
+++ din-ballyhoo-app/backend/controllers/albumController.js
@@ -4,7 +4,17 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getAllAlbums = factory.getAll(Album);
-exports.getAlbum = factory.getOne(Album);
+exports.getAlbum = factory.getOne(Album, [
+  {
+    path: 'ratings', // Virtual field for ratings
+    select: 'rating fan createdAt', // Only include these fields in the populated ratings
+  },
+  {
+    path: 'favorites', // Virtual field for favorites
+    select: 'fan createdAt', // Only include these fields in the populated favorites
+  },
+]);
+
 exports.createAlbum = factory.createOne(Album);
 exports.updateAlbum = factory.updateOne(Album);
 exports.deleteAlbum = factory.deleteOne(Album);
