1789311034401fd26a1a235eb8a1cc92b5e34522
Josh Endemann <jahend44@gmail.com>
2024-12-17 19:01:17 -0600

    Update webcastController.js
    Add Ratings and Favorites

--- din-ballyhoo-app/backend/controllers/webcastController.js
+++ din-ballyhoo-app/backend/controllers/webcastController.js
@@ -4,7 +4,16 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getAllWebcasts = factory.getAll(Webcast);
-exports.getWebcast = factory.getOne(Webcast);
+exports.getWebcast = factory.getOne(Webcast, [
+  {
+    path: 'ratings', // Virtual field for ratings
+    select: 'rating fan createdAt', // Only include these fields in the populated ratings
+  },
+  {
+    path: 'favorites', // Virtual field for favorites
+    select: 'fan createdAt', // Only include these fields in the populated favorites
+  },
+]);
 exports.createWebcast = factory.createOne(Webcast);
 exports.updateWebcast = factory.updateOne(Webcast);
 exports.deleteWebcast = factory.deleteOne(Webcast);
