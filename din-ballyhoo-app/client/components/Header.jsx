1d4bfc94a5d410790391c4a3910f8e5480274445
Josh Endemann <jahend44@gmail.com>
2024-12-23 21:25:49 -0600

    Create Header.jsx
    Built first component in nextJS

---
+++ din-ballyhoo-app/client/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Din Ballyhoo</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>Login</li>
+          <li>Signup</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
