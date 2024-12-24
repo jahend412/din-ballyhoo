ccbcc8cd667a19d1bf11ab0739dc2b7d593fd9a6
Josh Endemann <jahend44@gmail.com>
2024-12-24 10:44:28 -0600

    Create page.js
    Create LoginPage

---
+++ din-ballyhoo-app/client/app/login/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <Header />
+      <h1>Login</h1>
+      <p>Log in to the Ballyhoo App</p>
+    </main>
+  );
+}
