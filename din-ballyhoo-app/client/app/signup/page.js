ddaf7ef9e9ac42c13cac62fd6eeed0b78decf5c2
Josh Endemann <jahend44@gmail.com>
2024-12-24 11:02:40 -0600

    Create page.js
    Create Signup page

---
+++ din-ballyhoo-app/client/app/signup/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function SignUpPage() {
+  return (
+    <main>
+      <Header />
+      <h1>Sign Up</h1>
+      <p>Sign up to the Din Ballyhoo App</p>
+    </main>
+  );
+}
