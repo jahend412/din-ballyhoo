9b01f53855cf9220e28879cf9c88682002783c37
Josh Endemann <jahend44@gmail.com>
2024-12-25 19:31:09 -0600

    Update Header.jsx
    Added Logo and created Links for log in and Sign up

--- din-ballyhoo-app/client/components/Header.jsx
+++ din-ballyhoo-app/client/components/Header.jsx
@@ -1,14 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
-      <h1>Din Ballyhoo</h1>
+      <Image src="/logo.png" alt="Ballyhoo App" width={150} height={150} />
       <nav>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <Link href="/">Home</Link>
           </li>
-          <li>Login</li>
-          <li>Signup</li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+          <li>
+            <Link href="/signup">Signup</Link>
+          </li>
         </ul>
       </nav>
     </header>
