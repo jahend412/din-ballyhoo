81935ad4a8e6b6ab2fc6272b2796606143a7a6a6
Josh Endemann <jahend44@gmail.com>
2024-12-26 20:40:01 -0600

    style: Designed the Header Component
    Added Styles from Header.module.css|

--- din-ballyhoo-app/client/components/Header.jsx
+++ din-ballyhoo-app/client/components/Header.jsx
@@ -1,22 +1,23 @@
-import Image from "next/image";
 import Link from "next/link";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <Image src="/logo.png" alt="Ballyhoo App" width={150} height={150} />
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-          <li>
-            <Link href="/signup">Signup</Link>
-          </li>
-        </ul>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src="/logo1.png" alt="Ballyhoo App" width="100" height="100" />
+      </div>
+      <h1 className={styles.title}>Din Ballyhoo </h1>
+      <nav className={styles.navLinks}>
+        <Link href="/" className={styles.navLink}>
+          Home
+        </Link>
+        <Link href="/about" className={styles.navLink}>
+          About
+        </Link>
+        <Link href="/contact" className={styles.navLink}>
+          Contact
+        </Link>
       </nav>
     </header>
   );
