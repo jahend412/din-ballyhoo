a00cd5304e4cd32c328d9fe492357890ae0a05d9
Josh Endemann <jahend44@gmail.com>
2025-01-01 19:52:55 -0600

    Create cardConfigs.js
    Create these configs in its own file to keep the welcome page clean.

---
+++ din-ballyhoo-app/client/app/config/cardConfigs.js
@@ -0,0 +1,26 @@
+export const albumConfig = {
+  imageField: "coverImage",
+  titleField: "title",
+  subtitleField: "artist",
+  detailField: ["releaseDate"],
+  linkField: "_id",
+  linkBase: "/albums",
+};
+
+export const showConfig = {
+  imageField: "coverImage",
+  titleField: "title",
+  subtitleField: "venue",
+  detailField: ["data", "location"],
+  linkField: "_id",
+  linkBase: "/shows",
+};
+
+export const webcastConfig = {
+  imageField: "coverImage",
+  titleField: "title",
+  subtitleField: "date",
+  detailField: ["location"],
+  linkField: "_id",
+  linkBase: "/webcasts",
+};
