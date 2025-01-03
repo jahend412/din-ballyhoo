24c0c69f9690026b36f2bb29f47ab9f43b41e756
Josh Endemann <jahend44@gmail.com>
2025-01-02 21:44:28 -0600

    Fix: Had to allow images to be pulled from localhost

--- din-ballyhoo-app/client/next.config.js
+++ din-ballyhoo-app/client/next.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+      },
+    ],
+  },
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: "http://localhost:8080/api/:path*", // This rewrites API calls to your backend server
       },
     ];
   },
