67cd6b895cbf8b967ef88b08573d5c0a59017231
Josh Endemann <jahend44@gmail.com>
2025-01-02 19:42:07 -0600

    Update albumRoutes.js
    Added uploadCoverImage Middleware

--- din-ballyhoo-app/backend/routes/albumRoutes.js
+++ din-ballyhoo-app/backend/routes/albumRoutes.js
@@ -8,15 +8,25 @@ const router = express.Router();
 // Protect all routes below this middleware
 router.use(authController.protect);
 
+// Route to fetch all albums or create a new album with a cover image
 router
-  .route('/') // Specify the base path for both methods
+  .route('/')
   .get(checkPermissions('view-album'), albumController.getAllAlbums) // Fetch all albums
-  .post(checkPermissions('create-album'), albumController.createAlbum); // Add a new album
+  .post(
+    checkPermissions('create-album'),
+    albumController.uploadAlbumCover, // Middleware to handle file uploads
+    albumController.createAlbum // Create a new album
+  );
 
+// Routes for specific album operations
 router
-  .route('/:id') // Specify the path for operations on a specific album by ID
+  .route('/:id')
   .get(checkPermissions('view-album'), albumController.getAlbum) // Get a specific album by ID
-  .patch(checkPermissions('edit-album'), albumController.updateAlbum) // Update album by ID
+  .patch(
+    checkPermissions('edit-album'),
+    albumController.uploadAlbumCover, // Middleware for updating the album's cover image
+    albumController.updateAlbum // Update album details
+  )
   .delete(checkPermissions('delete-album'), albumController.deleteAlbum); // Delete album by ID
 
 module.exports = router;
