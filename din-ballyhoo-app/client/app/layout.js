774377964baf340349b9fdd3828cf8133bcd8946
Josh Endemann <jahend44@gmail.com>
2025-01-07 19:01:27 -0600

    Update layout.js
    Added UserProvider to start pushing the user across the entire site.

--- din-ballyhoo-app/client/app/layout.js
+++ din-ballyhoo-app/client/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { UserProvider } from "@/app/context/UserContext";
 
 export const metadata = {
   title: "Din Ballyhoo",
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <UserProvider>{children}</UserProvider>
+      </body>
     </html>
   );
 }
