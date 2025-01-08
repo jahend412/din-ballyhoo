083adcf009ca16a50edce0603e458e550d4401d7
Josh Endemann <jahend44@gmail.com>
2025-01-07 21:36:34 -0600

    Update Header.jsx
    Temporary removal of user in the Header

--- din-ballyhoo-app/client/components/Header.jsx
+++ din-ballyhoo-app/client/components/Header.jsx
@@ -1,19 +1,14 @@
 import Link from "next/link";
+
 import styles from "./Header.module.css";
 
-export default function Header({ user }) {
+export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Din Ballyhoo </h1>
+      <h1 className={styles.title}>Din Ballyhoo</h1>
       <nav className={styles.navLinks}>
-        <Link href={"/profile/${userId}"} className={styles.navLink}>
-          Welcome, {user.name}!
-        </Link>
-        <Link href="/logout" className={styles.navLink}>
-          Logout
-        </Link>
-        <Link href="/news" className={styles.navLink}>
-          News
+        <Link href="/" className={styles.navLink}>
+          Home
         </Link>
         <Link href="/shows" className={styles.navLink}>
           Shows
@@ -27,8 +22,8 @@ export default function Header({ user }) {
         <Link href="/about" className={styles.navLink}>
           About
         </Link>
-        <Link href="/merch" className={styles.navLink}>
-          Store
+        <Link href="/logout" className={styles.navLink}>
+          Logout
         </Link>
       </nav>
     </header>
