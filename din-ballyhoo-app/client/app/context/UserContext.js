719c8930aa16104a1667e361c53bc55c22b540f8
Josh Endemann <jahend44@gmail.com>
2025-01-09 19:20:31 -0600

    Update UserContext.js
    Added a check in the loadUserFromToken function to validate if the JWT token has expired. If the token is expired, it removes the token from cookies, sets the user to null, and stops further processing.

--- din-ballyhoo-app/client/app/context/UserContext.js
+++ din-ballyhoo-app/client/app/context/UserContext.js
@@ -25,21 +25,31 @@ const UserProvider = ({ children }) => {
 
   const loadUserFromToken = async () => {
     const token = Cookies.get("token");
+    console.log("Token:", token);
 
     if (token) {
       try {
         const decoded = parseJwt(token);
-        const userId = decoded.id;
+        const currentTime = Date.now() / 1000;
 
+        if (decoded.exp < currentTime) {
+          console.warn("Token has expired");
+          Cookies.remove("token");
+          setUser(null);
+          setLoading(false);
+          return;
+        }
+
+        const userId = decoded.id;
         const response = await axios.get(`/api/v1/users/${userId}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-
+        console.log("API response:", response.data);
         setUser(response.data.data.user);
       } catch (error) {
-        console.error("Error fetching user:", error);
+        console.error("Error fetching user or decoding token:", error);
         setUser(null);
       }
     } else {
@@ -50,11 +60,13 @@ const UserProvider = ({ children }) => {
 
   const loginUser = (user, token) => {
     Cookies.set("token", token);
+    console.log("Token set:", token);
     setUser(user);
   };
 
   const logoutUser = () => {
     Cookies.remove("token");
+    console.log("Token removed", Cookies.get("token"));
     setUser(null);
   };
 
