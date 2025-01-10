fb27c79b832e60ed26cc2a9cf70b3032d8a0636c
Josh Endemann <jahend44@gmail.com>
2025-01-09 20:16:20 -0600

    Add news schema and model for news articles

---
+++ din-ballyhoo-app/backend/models/newsModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const newsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  content: {
+    type: String,
+    required: [true, 'Content is required'],
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  datePosted: {
+    type: Date,
+    required: false,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+});
+
+const News = mongoose.model('News', newsSchema);
+module.exports = News;
