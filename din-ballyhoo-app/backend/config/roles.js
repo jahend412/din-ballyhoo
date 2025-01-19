9a0ced609950b8bddd0bdacee1a59302ebeae36a
Josh Endemann <jahend44@gmail.com>
2025-01-19 12:18:43 -0600

    Update roles.js
    Apply view-news to each role

--- din-ballyhoo-app/backend/config/roles.js
+++ din-ballyhoo-app/backend/config/roles.js
@@ -16,6 +16,7 @@ const roles = {
     permissions['update-password'],
     permissions['view-user'],
     permissions['view-comments'],
+    permissions['view-news'],
   ],
   bandMember: [
     permissions['create-comment'],
@@ -42,6 +43,7 @@ const roles = {
     permissions['view-dashboard'],
     permissions['view-user'],
     permissions['view-comments'],
+    permissions['view-news'],
   ],
   admin: [
     permissions['create-comment'],
@@ -82,6 +84,7 @@ const roles = {
     permissions['delete-user'],
     permissions['create-track'],
     permissions['create-news'],
+    permissions['view-news'],
     permissions['edit-news'],
     permissions['delete-news'],
     permissions['view-comments'],
