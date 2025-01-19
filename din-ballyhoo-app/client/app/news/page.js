bb691e139969b06ec9a4d40329267b410aa4ce8a
Josh Endemann <jahend44@gmail.com>
2025-01-19 17:17:01 -0600

    Update page.js
    - Made the news page functional.  Adding in the Cards for the news.

--- din-ballyhoo-app/client/app/news/page.js
+++ din-ballyhoo-app/client/app/news/page.js
@@ -1,28 +1,56 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import { fetchNews } from "@/app/utils/fetchEntity";
+import { newsConfig } from "@/app/config/cardConfigs";
+import styles from "./NewsPage.module.css";
 
-const NewsPage = () => {
+export default function NewsPage() {
   const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+  const token = localStorage.getItem("token");
 
+  // Fetch news
   useEffect(() => {
-    // Fetch the news from your API
-    fetch("http://localhost:8080/api/v1/news")
-      .then((res) => res.json())
-      .then((data) => setNews(data.data));
-  }, []);
+    const loadNews = async () => {
+      try {
+        const fetchedNews = await fetchNews(token);
+        setNews(fetchedNews);
+      } catch (err) {
+        console.error("Error fetching news:", err);
+        setError("Error fetching news");
+      }
+    };
 
+    loadNews();
+  }, [token]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (news.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  console.log(news);
   return (
     <div>
-      <h1>Latest News</h1>
-      {news.map((item) => (
-        <div key={item._id}>
-          <h2>{item.title}</h2>
-          <p>{item.content}</p>
+      <Header />
+      <div className={styles.newsPage}>
+        <h2>News</h2>
+        <div className={styles.cardContainer}>
+          {news.length > 0 ? (
+            news.map((news) => (
+              <Card key={news._id} data={news} config={newsConfig} />
+            ))
+          ) : (
+            <p>No news found</p>
+          )}
         </div>
-      ))}
+      </div>
     </div>
   );
-};
-
-export default NewsPage;
+}
