f96921064746c5b0c0df28cafc76267b13b6bf4b
Josh Endemann <jahend44@gmail.com>
2025-01-19 17:16:15 -0600

    Update page.js
    Added the News Cards into the Home page.  Now the user before logging on will be able to check out the news stories from the band and login or sign up

--- din-ballyhoo-app/client/app/page.js
+++ din-ballyhoo-app/client/app/page.js
@@ -1,16 +1,55 @@
+"use client";
+
 import Link from "next/link";
-import Image from "next/image";
+import Card from "@/components/Card";
+import { newsConfig } from "@/app/config/cardConfigs";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNewsData = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/api/v1/news");
+        const data = await res.json();
+
+        if (data.status === "success") {
+          setNews(data); // Store the entire response object
+        } else {
+          setError("Failed to fetch news data");
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNewsData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <main className="home-container">
-      <Image
-        className="home-logo"
-        src="/logo.png"
-        alt="Ballyhoo App"
-        layout="fill"
-        objectFit="contain"
-      />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <h1 className="home-title">Din Ballyhoo</h1>
+      <h2 className="home-subtitle"> News </h2>
+      <div className="home-news">
+        {news.data && news.data.length > 0 ? (
+          news.data.map((newsItem) => (
+            <Card key={newsItem._id} data={newsItem} config={newsConfig} />
+          ))
+        ) : (
+          <p>No news found</p>
+        )}
+      </div>
+      <h2 className="home-subtitle"> Log in to check out our Music </h2>
       <div className="home-links">
         <Link href="/login" className="home-link">
           Log In
