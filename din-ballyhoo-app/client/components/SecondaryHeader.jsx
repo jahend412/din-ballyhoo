52bc609b62f7acf9d1416138a4f158a6b61c8d90
Josh Endemann <jahend44@gmail.com>
2025-01-20 10:04:25 -0600

    Update SecondaryHeader.jsx
    - Changed styles import.
    Added a subtitle

--- din-ballyhoo-app/client/components/SecondaryHeader.jsx
+++ din-ballyhoo-app/client/components/SecondaryHeader.jsx
@@ -1,23 +1,21 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./Header.module.css";
+import styles from "./SecondaryHeader.module.css";
 
 export default function SecondaryHeader() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Din Ballyhoo</h1>
-      <nav className={styles.navLinks}>
+      <h2 className={styles.subtitle}>Check Out our Music</h2>
+      <div className={styles.navLinks}>
         <Link href="/login" className={styles.navLink}>
           Log In
         </Link>
         <Link href="/signup" className={styles.navLink}>
           Sign Up
         </Link>
-        <Link href="/" className={styles.navLink}>
-          Home
-        </Link>
-      </nav>
+      </div>
     </header>
   );
 }
