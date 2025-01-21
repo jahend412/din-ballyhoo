892a260e074a84da11b4df9d94a54fd1d3782d16
Josh Endemann <jahend44@gmail.com>
2025-01-21 12:14:50 -0600

    Update sendEmail.js
    Added Debugging and put in a try catch block for better error handling

--- din-ballyhoo-app/backend/utils/sendEmail.js
+++ din-ballyhoo-app/backend/utils/sendEmail.js
@@ -1,28 +1,35 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Activate in gmail "less secure app" option
-  });
+  try {
+    // 1) Create a transporter
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      logger: true,
+      debug: true,
+      // Activate in gmail "less secure app" option
+    });
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Natours',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+    // 2) Define the email options
+    const mailOptions = {
+      from: `'Din Ballyhoo' <${process.env.EMAIL_USERNAME}>`,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+      // html:
+    };
 
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
+    // 3) Actually send the email
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw new Error('There was an error sending the email. Try again later!');
+  }
 };
 
 module.exports = sendEmail;
