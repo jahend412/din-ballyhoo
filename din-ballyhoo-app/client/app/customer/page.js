2ea6115f9f5d2a6074956b9abb51a14262a2abc6
Josh Endemann <jahend44@gmail.com>
2025-01-22 20:29:18 -0600

    Built and Styled a very simple Customer Service Page
    Acts more like a Contact Page

---
+++ din-ballyhoo-app/client/app/customer/page.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import styles from "@/app/customer/CustomerService.module.css";
+
+export default function CustomerService() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Customer Service - Din Ballyhoo</title>
+        <meta
+          name="description"
+          content="Customer service page for Your Din Ballyhoo"
+        />
+      </Head>
+
+      <header className={styles.header}>
+        <h1>Customer Service</h1>
+      </header>
+
+      <section className={styles.contact}>
+        <h2>Contact Us</h2>
+        <p className={styles.contactInfo}>
+          If you have any questions or feedback, please feel free to reach out
+          via email at joshendemann@gmail.com. I appreciate your feedback and
+          will get back to you as soon as possible. Thank you for your support!
+        </p>
+
+        <p className={styles.contactInfo}>
+          Please note that this website is a project created to showcase web
+          development skills and is part of an ongoing learning process in
+          coding. It is not intended for commercial purposes at this time.
+        </p>
+      </section>
+    </div>
+  );
+}
