2ca72dd8a7c88e803ccb0c85ed291d36f1b45246
Josh Endemann <jahend44@gmail.com>
2025-01-26 17:40:39 -0600

    Add 'use client'

--- din-ballyhoo-app/client/app/resetPassword/page.js
+++ din-ballyhoo-app/client/app/resetPassword/page.js
@@ -1,5 +1,7 @@
+"use client";
+
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function ResetPassword() {
